c1ebba00309b118b408358a20c84981e32cc3048
Aronis Mariano <aox.mariano@gmail.com>
2015-05-20 00:38:14 -0400

    added user, email, log apis

---
+++ api/controllers/EmailController.js
@@ -0,0 +1,11 @@
+/**
+ * EmailController
+ *
+ * @description :: Server-side logic for managing emails
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
