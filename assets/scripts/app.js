4bc45043cb7b26fd80253684635051be63e48982
Aronis Mariano <aox.mariano@gmail.com>
2015-12-31 01:57:42 -0400

    fixed tabs on app.js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -17,41 +17,44 @@ angular
   .config(function($stateProvider, $urlRouterProvider, $authProvider) {
 	
 	$authProvider.configure({
-		apiUrl: '/user',
-		emailSignInPath: '/login',
-		tokenValidationPath: '/me',
+      	apiUrl: '/user',
+      	emailSignInPath: '/login',
+      	tokenValidationPath: '/me',
+        storage:                 'cookies',
+        forceValidateToken:      false,
+        validateOnPageLoad:      true,
 	});
 	
     $urlRouterProvider.when('/dashboard', '/dashboard/overview');
     $urlRouterProvider.otherwise('/login');
 
     $stateProvider
-      .state('base', {
-        abstract: true,
-        url: '',
-        templateUrl: 'views/base.html'
-      })
-        .state('login', {
-          url: '/login',
-          parent: 'base',
-          templateUrl: 'views/login.html',
-          controller: 'LoginCtrl'
+        .state('base', {
+            abstract: true,
+            url: '',
+            templateUrl: 'views/base.html'
         })
-        .state('dashboard', {
-          url: '/dashboard',
-          parent: 'base',
-          templateUrl: 'views/dashboard.html',
-          controller: 'DashboardCtrl'
-        })
-          .state('overview', {
-            url: '/overview',
-            parent: 'dashboard',
-            templateUrl: 'views/dashboard/overview.html'
-          })
-          .state('reports', {
-            url: '/reports',
-            parent: 'dashboard',
-            templateUrl: 'views/dashboard/reports.html'
-          });
+            .state('login', {
+                url: '/login',
+                parent: 'base',
+                templateUrl: 'views/login.html',
+                controller: 'LoginCtrl'
+            })
+            .state('dashboard', {
+                url: '/dashboard',
+                parent: 'base',
+                templateUrl: 'views/dashboard.html',
+                controller: 'DashboardCtrl'
+            })
+                .state('overview', {
+                    url: '/overview',
+                    parent: 'dashboard',
+                    templateUrl: 'views/dashboard/overview.html'
+                })
+                .state('reports', {
+                    url: '/reports',
+                    parent: 'dashboard',
+                    templateUrl: 'views/dashboard/reports.html'
+                });
 
   });
